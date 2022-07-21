da4d960faee97240c31c25bb8b82183c104cc26d
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-07-21 14:52:48 +0300

    uzd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Fun from './Components/006/Fun';
 import { useState } from 'react';
 import Sad from './Components/006/Sad';
+import randColor from './Functions/randColor';
 
 
 
@@ -9,12 +10,22 @@ function App() {
 
   const [count, setCount] = useState(0);
 
+  const [kv, setKv] = useState([]);
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Labas</h1>
+          <h1 onClick={() => setKv(k => [...k, randColor()])}>State</h1>
           <Fun spalva='pink' setCount={setCount}></Fun>
           <Sad count={count}></Sad>
+          <div className="kv-bin">
+                {
+                    kv.map((c, i) => <div className="kv" style={{
+                        backgroundColor: c+'69',
+                        borderColor: c
+                    }} key={i}></div>)
+                }
+            </div>
         </header>
       </div>
     );
